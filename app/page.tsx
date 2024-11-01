34a5468888a26951e799fdf144210f55c87f4aa2
Daniel <56930814+w1qdev@users.noreply.github.com>
2024-11-02 00:52:57 +0500

    Add dynamic import for pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,28 @@
+import dynamic from "next/dynamic";
+
 import Hero from "./components/Hero/Hero";
 import AboutMe from "./components/AboutMe/about-me";
-import FeedBack from "./components/FeedBack/feedback";
-import WomanHappiness from "./components/WomanHappiness/woman-happiness";
-import IfYou from "./components/IfYou/IfYou";
-import MaraphoneProgram from "./components/MaraphoneProgram/MaraphoneProgram";
-import AfterMaraphone from "./components/AfterMaraphone/AfterMaraphone";
-import HowMaraphoneHappens from "./components/HowMaraphoneHappens/HowMaraphoneHappens";
-import Price from "./components/Price/Price";
-import LoveToMySelf from "./components/LoveToMyself/LoveToMyself";
-import PaintYourLove from "./components/PaintYourLove/PaintYourLove";
+
+const LoveToMySelf = dynamic(
+    () => import("./components/LoveToMyself/LoveToMyself")
+);
+const WomanHappiness = dynamic(
+    () => import("./components/WomanHappiness/woman-happiness")
+);
+const IfYou = dynamic(() => import("./components/IfYou/IfYou"));
+const MaraphoneProgram = dynamic(
+    () => import("./components/MaraphoneProgram/MaraphoneProgram")
+);
+const PaintYourLove = dynamic(
+    () => import("./components/PaintYourLove/PaintYourLove")
+);
+const Price = dynamic(() => import("./components/Price/Price"));
+const HowMaraphoneHappens = dynamic(
+    () => import("./components/HowMaraphoneHappens/HowMaraphoneHappens")
+);
+const AfterMaraphone = dynamic(
+    () => import("./components/AfterMaraphone/AfterMaraphone")
+);
 
 export default function Home() {
     return (
